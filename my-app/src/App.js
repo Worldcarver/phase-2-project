a6c9bf71c212102dc4ea7dd68bc4e0ec172a8e19
Worldcarver <ian.murphy92@gmail.com>
2022-06-17 21:38:48 -0400

    Removed unneeded files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd731705817mshe230c4e09e3f014p16249djsn407579c5d12d',
+		'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+	}
+};
+// need to set json server to different port? how?
+// send fetch to external api, and send post to json to capture data, inerpolate the data?
+// pull data from json not the api
+// requests to send data right to json and then render from there
+
+fetch(`https://travel-advisor.p.rapidapi.com/locations/v2/auto-complete?query=new%20york&lang=en_US&units=km`, options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
 
 function App() {
   return (
