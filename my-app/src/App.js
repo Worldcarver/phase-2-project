53e725dce0a867253c85e696defa61662e6a060a
Worldcarver <ian.murphy92@gmail.com>
2022-06-17 22:14:47 -0400

    Added Child branches

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,41 +2,43 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect } from 'react';
 
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'd731705817mshe230c4e09e3f014p16249djsn407579c5d12d',
-		'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-	}
-};
+
 // need to set json server to different port? how?
 // send fetch to external api, and send post to json to capture data, inerpolate the data?
 // pull data from json not the api
 // requests to send data right to json and then render from there
 
-fetch(`https://travel-advisor.p.rapidapi.com/locations/v2/auto-complete?query=new%20york&lang=en_US&units=km`, options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-
 
 function App() {
+
+
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'd731705817mshe230c4e09e3f014p16249djsn407579c5d12d',
+        'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+      }
+    };
+  fetch(`https://travel-advisor.p.rapidapi.com/locations/v2/auto-complete?query=las%20vegas&lang=en_US&units=km`, options)
+	  .then(response => response.json())
+	  .then(response => console.log(response))
+	  .catch(err => console.error(err));
+
+
+  
+   fetch('http://localhost:8000/locations'),{
+     method: 'POST',
+     headers: { 'Content-Type': 'application/json'},
+     body: JSON.stringify({title: 'test'})
+  }
+
+}, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
