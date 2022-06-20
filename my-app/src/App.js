a54c72dd5e9cea4f4e3325e5042f52ba124af248
Worldcarver <ian.murphy92@gmail.com>
2022-06-20 18:16:12 -0400

    Tested cards

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import SearchBar from './Components/SearchBar';
 import Header from './Components/Header'
 import DestinationDisplay from './Components/DesitnationDisplay'
-import DestinationList from './Components/DestinationList'
+import DestinationList from './Components/DestinationList';
+//import DestinationList from './Components/DestinationList'
 
 // need to set json server to different port? how?
 // send fetch to external api, and send post to json to capture data, inerpolate the data?
@@ -13,7 +14,7 @@ import DestinationList from './Components/DestinationList'
 function App() {
 
   const [destinations, setDestinations] = useState([])
-  const [searchInput, setSearchInput] = useState("")
+  //const [searchInput, setSearchInput] = useState("")
 
   useEffect(() => {
 
@@ -21,7 +22,7 @@ function App() {
     //post needs a submit button to work?
     //const destination = 'las vegas'
 
-    //https://travel-advisor.p.rapidapi.com/locations/v2/auto-complete?query=${destination}&lang=en_US&units=km
+    // https://travel-advisor.p.rapidapi.com/locations/v2/auto-complete?query=${destination}&lang=en_US&units=km
     // , options
 
 
@@ -46,7 +47,8 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <SearchBar setSearchInput={setSearchInput} destinations = {destinations}  />
+      <SearchBar destinations = {destinations}  />
+      <DestinationList destinations = {destinations} />
     </div>
   );
 }
