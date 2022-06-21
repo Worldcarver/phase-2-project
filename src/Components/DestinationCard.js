28a4963f27d60b866d346c7d08a4f790ac2c66ee
Worldcarver <104173152+Worldcarver@users.noreply.github.com>
2022-06-21 11:43:27 -0400

    Revert "Ian working"

--- src/Components/DestinationCard.js
+++ src/Components/DestinationCard.js
@@ -1,25 +1,7 @@
 import React from "react";
 
 
-function DestinationCard({ destination }) {
-
-    return (
-        <li className="card">
-            <div className="image">
-                <span className="price"></span>
-                <img src={destination.image} alt={destination.description} />
-            </div>
-            <div className="details">
-                {true ? (
-                    <button className="emoji-button favorite active">⭐</button>
-                ) : (
-                    <button className="emoji-button favorite">⭐</button>
-                )}
-                <strong>{destination.description}</strong>
-                <span></span>
-            </div>
-        </li>
-    )
+function DestinationCard(){
 
 }
 
