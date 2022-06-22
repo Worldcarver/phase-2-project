a02c3dc04426f7efd074c09dc167b263e8046416
Worldcarver <ian.murphy92@gmail.com>
2022-06-22 17:47:32 -0400

    Fixed CSS

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,7 @@ import DestinationDisplay from './Components/DesitnationDisplay'
 import DestinationList from './Components/DestinationList';
 import NewDestinationForm from './Components/NewDesitnationForm';
 import { Switch, Route, NavLink } from "react-router-dom"
-import {BrowserRouter as Router} from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom"
 
 // need to set json server to different port? how?
 // send fetch to external api, and send post to json to capture data, inerpolate the data?
@@ -19,56 +19,49 @@ function App() {
   const [favDest, setFavDest] = useState([])
 
   useEffect(() => {
- 
+
     fetch(`http://localhost:8000/locations`)
-	    .then(response => response.json())
-	    .then(response => setDestinations(response))
-	    .catch(err => console.error(err));
+      .then(response => response.json())
+      .then(response => setDestinations(response))
+      .catch(err => console.error(err));
 
-}, [])
+  }, [])
 
-function destPicker(newDest){
-  if (favDest.includes(newDest) === false) {
-    setFavDest([...favDest, newDest])
-  } else {
-    const removeFavDest = favDest.filter((dest) => {
-      return dest !== newDest
-    })
-    setFavDest(removeFavDest)
+  function destPicker(newDest) {
+    if (favDest.includes(newDest) === false) {
+      setFavDest([...favDest, newDest])
+    } else {
+      const removeFavDest = favDest.filter((dest) => {
+        return dest !== newDest
+      })
+      setFavDest(removeFavDest)
+    }
+
   }
-
-}
 
   return (
     <div className="App">
-      
-     
+
+
       <Header />
-     
       <Router>
-      <nav>
-                <span id = "nav-1">
-                <NavLink to="/"> Home </NavLink>
-                </span>
-                <span id = "nav-2">
-                <NavLink to ="/NewDestinationForm">Add a Destination!</NavLink>
-                </span>
-                <span id = "nav-3">
-                <NavLink to ="/DestinationDisplay">Travel List!</NavLink>
-                </span>
-      </nav>
-      <Switch>
-        <Route exact path = "/">
-      <DestinationList destinations = {destinations} destPicker = {destPicker}/>
-      </Route>
-        <Route path ="/DestinationDisplay">
-          <DestinationDisplay destPicker = {destPicker} favDest = {favDest}/>
-        </Route>
-        <Route path ="/NewDestinationForm">
-          <NewDestinationForm setDestinations={setDestinations} />
-        </Route>
-    </Switch>
-    </Router>
+        <nav>
+          <NavLink className="home" to="/"> Home </NavLink>
+          <NavLink className="addDest" to="/NewDestinationForm">   Add a Destination!   </NavLink>
+          <NavLink className="travelList" to="/DestinationDisplay">   Travel List!   </NavLink>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+            <DestinationList destinations={destinations} destPicker={destPicker} />
+          </Route>
+          <Route path="/DestinationDisplay">
+            <DestinationDisplay destPicker={destPicker} favDest={favDest} />
+          </Route>
+          <Route path="/NewDestinationForm">
+            <NewDestinationForm setDestinations={setDestinations} />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
